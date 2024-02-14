b1995caefd1ae29466606d9f9c2edaae8b7fad7a
BalagwisJohnDenver <imbajhey@gmail.com>
2024-02-14 14:57:43 +0800

    commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/5ad1518180.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/infor.png" type="image/x-icon">
+    <title>Informatics</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="upper-logo">
+            <figure class="logo">
+                <img src="images/info logo.png" alt="logo">
+            </figure>
+            <div class="name">
+                <div class="informatics">Informatics</div>
+                <div class="college">COLLEGE</div>
+            </div>
+        </nav>
+        <div class="hero-container">
+            <div class="hero-wrapper">
+                <div class="images-wrapper">
+                    <img id="ms" src="images/Mr. & Ms. (1).png" alt="hero-image">
+                </div>
+                <div class="main-content">
+                    <h3 class="search">Search for </h3>
+                    <h2 class="mrandms">Mr & Ms</h2>
+                    <h1 class="icon">Icon</h1>
+                </div>
+
+                <a class="get-started-btn">Get Started</a>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
