b65a8080e0b2672403e861419d1aed03a09c3104
BalagwisJohnDenver <imbajhey@gmail.com>
2024-02-15 15:14:27 +0800

    commit

--- image-slider.php
+++ image-slider.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/image-slider.css">
+
 </head>
 
 <body>
@@ -21,44 +22,10 @@
         </nav>
         <div class="box">
             <h4 class="category-text">Production Number (25%)</h4>
-            <div class="carousel">
-                <div class="carousel-item">
-                    <a href="#">
-                        <img src="images/dog.jpg" alt="Dog Image">
-                    </a>
-                    <h4>Responsive image gallery</h4>
-                </div>
 
-                <div class="carousel-item">
-                    <a href="#">
-                        <img src="images/dog.jpg" alt="Dog Image">
-                    </a>
-                    <h4>Tab image gallery</h4>
-                </div>
 
-                <div class="carousel-item">
-                    <a href="#">
-                        <img src="images/dog.jpg" alt="Dog Image">
-                    </a>
-                    <h4>Building header of a website</h4>
-                </div>
-
-                <div class="carousel-item">
-                    <a href="#">
-                        <img src="images/dog.jpg" alt="Dog Image">
-                    </a>
-                    <h4>image slider</h4>
-                </div>
-
-                <div class="carousel-item">
-                    <a href="#">
-                        <img src="images/dog.jpg" alt="Dog Image">
-                    </a>
-                    <h4>image lightbox gallery</h4>
-                </div>
-            </div>
         </div>
-    </div>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -33,25 +33,11 @@
                 </div>
 
                 <a href="candidate-pictures.php" class="get-started-btn">Get Started</a>
-                <!-- <div id="spotlight"></div> -->
 
             </div>
         </div>
     </div>
-    <!-- <script>
-        const spotlightEl = document.querySelector("#spotlight");
 
-        function handleMouseMove(event) {
-            const {
-                clientX,
-                clientY
-            } = event;
-
-            spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #000000ee 350px)`;
-        }
-
-        document.addEventListener("mousemove", handleMouseMove)
-    </script> -->
 </body>
 
 </html>
