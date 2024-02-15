2129926c968fed5dcc588e7e41fec9480d90902f
BalagwisJohnDenver <imbajhey@gmail.com>
2024-02-15 13:14:35 +0800

    commit

--- image-slider.php
+++ image-slider.php
@@ -20,7 +20,43 @@
             </div>
         </nav>
         <div class="box">
-            <h4 class="category-text">Production Number</h4>
+            <h4 class="category-text">Production Number (25%)</h4>
+            <div class="carousel">
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200130114641/responsivegallary1.png">
+                    </a>
+                    <h4>Responsive image gallery</h4>
+                </div>
+
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20191205124138/5510.png">
+                    </a>
+                    <h4>Tab image gallery</h4>
+                </div>
+
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/Screenshot-25-1024x453.png">
+                    </a>
+                    <h4>Building header of a website</h4>
+                </div>
+
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200214124104/imageslider1.png">
+                    </a>
+                    <h4>image slider</h4>
+                </div>
+
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200320161020/Screenshot-from-2020-03-20-16-06-36.png">
+                    </a>
+                    <h4>image lightbox gallery</h4>
+                </div>
+            </div>
         </div>
     </div>
 </body>
